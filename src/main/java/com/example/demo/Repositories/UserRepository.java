5813dd938d45c4f96f0ed9ae943f23186bf45ad9
Irena <i.ilvesten@hotmail.com>
2021-04-22 10:00:57 +0200

    minor fixes

--- src/main/java/com/example/demo/Repositories/UserRepository.java
+++ src/main/java/com/example/demo/Repositories/UserRepository.java
@@ -1,11 +1,12 @@
 package com.example.demo.Repositories;
 
 import com.example.demo.Enteties.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     public User findByUsername(String username);
 }
